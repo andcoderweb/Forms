1f94b07b59dffa8fb3289ef03f1991e2de0cc124
andcoderweb <andersoncoderweb@gmail.com>
2020-04-05 07:28:51 -0300

    Windows Forms exercicio tela login e cadastro

---
+++ Forms/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+            DateTime dataNascimento = dateTimePicker1.Value;
+            string cidade = comboBox1.Text;
+            bool sexo1, sexo2;
+            sexo1 = raiomas.Checked;
+            sexo2 = radioButton2.Checked;
+            int numeroCadastro =Convert.ToInt16(textBox2.Text);
+            var res = $"Nome:{nome},Data-Nas:{dataNascimento},Cidade:{cidade},Sexo:{sexo1},{sexo2},Numero Cadastro{numeroCadastro}";
+            MessageBox.Show(res, "Aviso", MessageBoxButtons.OK);
+        }
+    }
+}
+
---
+++ Forms/Form3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace Calculadora
+{
+    public partial class Form3 : Form
+    {
+        Thread nt;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Anderson" && textBox2.Text == "1234")
+            {
+                this.Close();
+                nt = new Thread(novoform);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
+            }
+
+
+            else
+                MessageBox.Show("Usuario ou senha errados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void novoform()
+        {
+            Application.Run(new Form4());
+        }
+    }
+}
